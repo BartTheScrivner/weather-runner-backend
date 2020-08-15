3ac9a77a3292018fbe5df4f04889b703a7675989
BartTheScrivner <dframe22@gmail.com>
2020-08-15 12:56:28 -0400

    building controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+  def create
+    user = 
+  end
+
+  def show
+    user = 
+  end
+
+  def update
+    user = 
+  end
+
+  def destroy
+    user = 
+  end
+
+  private 
+
+  def user_params
+    params.require(:user)
+    .permit(:name, :email, :password, :password_confirmation, :weekly_run_quota, :location)
+  end
 end
