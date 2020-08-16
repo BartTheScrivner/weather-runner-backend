bee9915d702fcec74f3f0e12922a1ec784d20184
BartTheScrivner <dframe22@gmail.com>
2020-08-16 16:17:40 -0400

    migration for user_preferences table (user_id)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_224628) do
+ActiveRecord::Schema.define(version: 2020_08_16_201509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2020_08_14_224628) do
     t.string "cloud_cover"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_user_preferences_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -60,4 +62,5 @@ ActiveRecord::Schema.define(version: 2020_08_14_224628) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "user_preferences", "users"
 end
