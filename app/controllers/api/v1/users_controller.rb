cb5fefadabe87e4ab5936e522ee3447ae5274edc
BartTheScrivner <dframe22@gmail.com>
2020-08-16 23:10:20 -0400

    added jwt auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,24 +1,30 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :logged_in?, only: [:create]
   def create
-    user = 
+    user = User.new(user_params)
+    if user.save
+      render json: {user: UserSerializer.new(user), token: encode_token({user_id: user.id})}
+    else
+      render json: {errors: "OH NO! > o < "}
+    end
   end
 
   def show
-    user = 
+
   end
 
   def update
-    user = 
+
   end
 
   def destroy
-    user = 
+
   end
 
   private 
 
   def user_params
-    params.require(:user)
-    .permit(:name, :email, :password, :password_confirmation, :weekly_run_quota, :location)
+    params.permit(:name, :email, :password, :password_confirmation, :weekly_run_quota, :location)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :user_preferences
-      resources :friendships
-      resources :runs
-      resources :users
+      resources :user_preferences, 
+      resources :friendships, 
+      resources :runs, 
+      resources :users, :except[:create]
     end
   end
+
+  post "/login", to: "auth#create"
+  post "/signup", to: "users#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
