10c06e418e23afcbee40ed9a8f011bf566fea57f
BartTheScrivner <dframe22@gmail.com>
2020-08-17 14:55:50 -0400

    fixed routes to auth controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :user_preferences, 
-      resources :friendships, 
-      resources :runs, 
+      resources :user_preferences 
+      resources :friendships 
+      resources :runs 
       resources :users, except: :create
     end
   end
 
-  post "/login", to: "auth#create"
-  post "/signup", to: "users#create"
+  post "api/v1/login", to: "api/v1/auth#create"
+  post "api/v1/signup", to: "api/v1/users#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
