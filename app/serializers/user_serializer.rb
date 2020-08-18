fcdec7be7368ad1c3796c2027097e70f5b7a6edd
BartTheScrivner <dframe22@gmail.com>
2020-08-17 21:42:56 -0400

    fixed bad serializer object problem

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  has_many :runs, :friends, :friend_requests
-  has_one :user_preference
   attributes :id, :name, :img_url, :location, :weekly_run_quota
+  has_many :runs
+  has_one :user_preference
 end
