88aee3295227254ddcfaa87e781b4b60d85bde63
BartTheScrivner <dframe22@gmail.com>
2020-08-19 09:31:25 -0400

    problem with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,15 +12,15 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50 }
   
   # EMAIL VALIDATIONS
-  before_save { self.email = email.downcase } 
+  # before_save { self.email = email.downcase } 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-  format: { with: VALID_EMAIL_REGEX },
-  uniqueness: { case_sensitive: false }
+  # validates :email, presence: true, length: { maximum: 255 },
+  # format: { with: VALID_EMAIL_REGEX },
+  # uniqueness: { case_sensitive: false }
   
   # PASSWORD VALIDATIONS
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  # validates :password, presence: true, length: { minimum: 6 }
 
       def friends
         self.frienders + self.friendees
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :img_url, :location, :weekly_run_quota
+  attributes :id, :name, :bio, :img_url, :location, :weekly_run_quota
   has_many :runs
   has_one :user_preference
 end
